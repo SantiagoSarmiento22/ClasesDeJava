32f21b514b26a7bb545c329a0686db4d794c3774
SantiagoSarmiento22 <125980675+SantiagoSarmiento22@users.noreply.github.com>
2024-05-28 09:44:47 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+/*package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        int num1=12;
+        int num2=13;
+        System.out.println(num1+num2);
+    }
+}
+
+ */
+
+/*package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        int numero1=12;
+        int numero2=13;
+        System.out.println(numero1/numero2);
+    }
+}
+ */
+
+/*
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero");
+        int num1=leer.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        int num2=leer.nextInt();
+        System.out.println("Ingrese el tercer numero");
+        int num3=leer.nextInt();
+        int promedio=(num1 + num2 + num3)/3;
+        System.out.println(promedio);
+    }
+}
+ */
