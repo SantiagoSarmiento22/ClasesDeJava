d0acc368432e9f3425f9ae3e763cbaeccede8bac
Esteban Hernandez <estebanhl@ext.inditex.com>
2024-07-03 14:05:43 -0500

    Provide Unit Tests

--- granmaRestaurant/granmaRestaurant/src/test/java/com/globant/granmaRestaurant/controllers/ComboControllerTest.java
+++ granmaRestaurant/granmaRestaurant/src/test/java/com/globant/granmaRestaurant/controllers/ComboControllerTest.java
@@ -16,41 +16,64 @@ import static org.mockito.Mockito.when;
 
 public class ComboControllerTest {
 
-    @Mock
-    private IComboService comboService;
+	@Mock
+	private IComboService comboService;
 
-    @InjectMocks
-    private ComboController comboController;
+	@InjectMocks
+	private ComboController comboController;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-    @Test
-    public void testCreateCombo() {
+	@Test
+	public void testCreateCombo() {
+		// Given
+		final ComboDTO comboDTO = new ComboDTO();
+		comboDTO.setFantasyName("Combo Pollito");
+		comboDTO.setDescription("Broaster con papas");
+		comboDTO.setPrice(18.99);
+		comboDTO.setAvailable(true);
+		comboDTO.setActive(true);
 
-        ComboDTO comboDTO = new ComboDTO();
-        comboDTO.setFantasyName("Combo Pollito");
-        comboDTO.setDescription("Broaster con papas");
-        comboDTO.setPrice(18.99);
-        comboDTO.setAvailable(true);
-        comboDTO.setActive(true);
+		final ComboDTO createdComboDTO = new ComboDTO();
+		createdComboDTO.setFantasyName("Combo Hamburguesa");
+		createdComboDTO.setDescription("Delicioso combo con hamburguesa y papas fritas");
+		createdComboDTO.setPrice(15.94);
+		createdComboDTO.setAvailable(true);
+		createdComboDTO.setActive(true);
+		when(this.comboService.createCombo(any(ComboDTO.class))).thenReturn(createdComboDTO);
+		// When
+		final ResponseEntity<ComboDTO> response = this.comboController.createCombo(comboDTO);
+		// Then
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertEquals(createdComboDTO, response.getBody());
+	}
 
-        ComboDTO createdComboDTO = new ComboDTO();
-        createdComboDTO.setFantasyName("Combo Hamburguesa");
-        createdComboDTO.setDescription("Delicioso combo con hamburguesa y papas fritas");
-        createdComboDTO.setPrice(15.94);
-        createdComboDTO.setAvailable(true);
-        createdComboDTO.setActive(true);
+	@Test
+	public void testGetCombo() {
+		// Given
+		final String comboIdTest = "1";
+		final ComboDTO createdComboDTO = new ComboDTO();
+		createdComboDTO.setFantasyName("Combo Hamburguesa");
+		createdComboDTO.setDescription("Delicioso combo con hamburguesa y papas fritas");
+		createdComboDTO.setPrice(15.94);
+		createdComboDTO.setAvailable(true);
+		createdComboDTO.setActive(true);
+		when(this.comboService.getCombo(comboIdTest)).thenReturn(createdComboDTO);
+		// When
+		final ResponseEntity<ComboDTO> combo = this.comboController.getCombo(comboIdTest);
+		// Then
+		assertEquals(HttpStatus.OK, combo.getStatusCode());
+		assertEquals(createdComboDTO, combo.getBody());
 
-        when(comboService.createCombo(any(ComboDTO.class))).thenReturn(createdComboDTO);
+	}
 
-
-        ResponseEntity<ComboDTO> response = comboController.createCombo(comboDTO);
-
-
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals(createdComboDTO, response.getBody());
-    }
 }
+
+// Fast
+// Independent
+// Repeatable
+// Self-Validating
+// Timely
