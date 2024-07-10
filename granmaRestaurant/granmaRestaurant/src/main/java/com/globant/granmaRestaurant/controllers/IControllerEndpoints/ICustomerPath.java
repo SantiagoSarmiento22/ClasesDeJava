7cb366ddf513733de79a1a14ab838fe4ef80e9b0
Esteban Hernandez <estebanhl@ext.inditex.com>
2024-07-10 16:24:10 -0500

    Adjust customer controller paths for RESTful API

--- granmaRestaurant/granmaRestaurant/src/main/java/com/globant/granmaRestaurant/controllers/IControllerEndpoints/ICustomerPath.java
+++ granmaRestaurant/granmaRestaurant/src/main/java/com/globant/granmaRestaurant/controllers/IControllerEndpoints/ICustomerPath.java
@@ -4,11 +4,7 @@ public interface ICustomerPath {
 
 
         String URL_BASE = "/customers";
-        String CREATE_CUSTOMER = "/create";
-        String GET_CUSTOMER = "/{document}";
-        String UPDATE_CUSTOMER = "/update/{document}";
-        String DELETE_CUSTOMER = "/delete/{document}";
-        String GET_LIST = "/getAllCustomers";
+        String CUSTOMER_ID_PARAM = "/{document}";
         String GET_LISTSORT ="/sortedCustomers";
 
 }
